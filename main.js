c1bb6259525b93d57505ca07cc12adea51a4a319
Victor Herrera <victocode007@gmail.com>
2024-08-04 20:26:36 -0400

    corrección de enlace

--- main.js
+++ main.js
@@ -19,7 +19,26 @@ function listarTareas() {
         alert(listaTareas);
     }
 }
-
+function completarTarea() {
+    listarTareas();
+    const indice = parseInt(prompt("Ingrese el número de la tarea que desea marcar como completada:")) - 1;
+    if (indice >= 0 && indice < tareas.length) {
+        tareas[indice].completada = true;
+        alert("Tarea marcada como completada.");
+    } else {
+        alert("Índice de tarea no válido.");
+    }
+}
+function eliminarTarea() {
+    listarTareas();
+    const indice = parseInt(prompt("Ingrese el número de la tarea que desea eliminar:")) - 1;
+    if (indice >= 0 && indice < tareas.length) {
+        tareas.splice(indice, 1);
+        alert("Tarea eliminada.");
+    } else {
+        alert("Índice de tarea no válido.");
+    }
+}
 function menu() {
     let opcion;
     do {
@@ -33,8 +52,10 @@ function menu() {
                 listarTareas();
                 break;
             case "3":
+                completarTarea();
                 break;
             case "4":
+                eliminarTarea();
                 break;
             case "5":
                 alert("Saliendo...");
